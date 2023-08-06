a05441738f07c00cfd39a491d083f0918739ede4
JoacssGL <141186589+JoacssGL@users.noreply.github.com>
2023-08-05 23:40:22 -0300

    agregado en el js

--- eventos.js
+++ eventos.js
@@ -2,3 +2,8 @@ const boton = document.querySelector("#boton");
 boton.addEventListener("click", function() {
     alert("Hola! Soy el div!");
 });
+
+const btn = document.querySelector(".boton");
+btn.removeEventListener("click", function() {
+    alert("Hola! Soy el div!");
+});
